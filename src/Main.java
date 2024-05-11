d393e1cfd01b0c27c83ce12e8c67ccb8dbd3caaa
ArnurAdilkhan <157271429+ArnurAdilkhan@users.noreply.github.com>
2024-05-12 00:35:08 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -34,13 +34,6 @@ public class Main {
         System.out.println(bst.size(bst.getRoot()));
 
     }
-    public static String generateRandomString(int length) {
-        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        StringBuilder result = new StringBuilder();
-        for(int i = 0 ; i < length ; i++) {
-            result.append(characters.charAt(random.nextInt(characters.length())));
-        }
-        return result.toString();
-    }
+    
 
 }
